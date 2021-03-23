e7836a04e81c79526adc39934f4282ca455bb6c9
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-23 20:20:56 -0300

    Finalizado a conexão com web socket - dia 2

---
+++ client/src/cliConfig.js
@@ -0,0 +1,34 @@
+export default class CliConfig
+{
+    constructor({ username, hostUri, room })
+    {
+        this.username = username;
+        this.room = room;
+        // traduz a string passada em uma uri, separando protocolo etc
+        const { hostname, port, protocol } = new URL(hostUri);
+
+        this.host = hostname;
+        this.port = port;
+        // Expressão remove todos caracteres que não forem letras
+        this.protocol = protocol.replace(/\W/, '');
+    }
+
+    static parseArguments(commands)
+    {
+        const cmd = new Map();
+        for(const key in commands)
+        {
+            const index = parseInt(key);
+            const command = commands[key];
+
+            const commandPrefix = "--";
+            if(!command.includes(commandPrefix)) continue;
+            cmd.set(
+                command.replace(commandPrefix, ""),
+                commands[index + 1]
+            );
+        }
+
+        return new CliConfig(Object.fromEntries(cmd));
+    }
+}
